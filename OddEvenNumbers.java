a1efdb1caf45ced171b3a6c6642ee782e8064ae1
Yagyata <yagyatasharmaa@gmail.com>
2025-03-01 13:41:38 +0530

    [Yagyata] ADD:Printing if the number is even or odd

--- OddEvenNumbers.java
+++ OddEvenNumbers.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class OddEvenNumbers {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         // Taking input from the user
