e53930fa99d9a3461116a18a8593793aafcac344
Yagyata <yagyatasharmaa@gmail.com>
2025-03-03 09:46:32 +0530

    [Yagyata] ADD:Check if a number is divisible by sum of its digits

---
+++ HarshadNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class HarshadNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        // Calculate the sum of its digits
+        int sum = 0;
+        int temp = number; // Store original number
+
+        while (temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+
+        // Check if the number is divisible by the sum of its digits
+        if (number % sum == 0) {
+            System.out.println(number + " is a Harshad Number.");
+        } else {
+            System.out.println(number + " is Not a Harshad Number.");
+        }
+        sc.close(); 
+    }
+}
